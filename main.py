50a9c7ec29e09240861d0f480eef74f5357c8406
Mohamed Adel <Mohamed.adel_mtc@outlook.com>
2025-04-06 03:14:24 +0200

    update

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #Entry Point that starts the application
 import sys
 import os 
-from models.database import session, engine, Base
-from models.employee_model import Employee
+#from Models.database import session, engine, Base
+from Models.employee_model import Employee
 
 #Create All the Tables in the Database
-Base.metadata.create_all(engine)
+#Base.metadata.create_all(engine)
 
 
 
